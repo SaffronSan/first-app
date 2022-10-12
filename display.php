c20f9020697d428a1c1933404148f4016918388a
Anish Thapa <94629230+SaffronSan@users.noreply.github.com>
2022-10-12 18:47:51 -0400

    Add files via upload

---
+++ display.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP_Display</title>
+</head>
+<body>
+    <?php 
+        $conn = new mysqli("localhost", "root", "password","umk");
+        if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+        $sql = "select * from localtable";
+        $result = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)) {
+                echo $row['id'] . $row['name']  . $row['number'] . "<br>";
+            }
+        }
+        mysqli_close($conn);
+    ?>
+</body>
+</html>
